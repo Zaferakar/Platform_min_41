7c5381d8dd9a4213afe4ca2820898812e7b0b50e
Zaferakar <zaferakar22@gmail.com>
2023-10-30 21:40:37 +0300

    Initial commit

---
+++ app/src/main/python/main.py
@@ -0,0 +1,14 @@
+analiz_batis_belirtec = "python_islemi_bitti"
+import sys
+import os
+from com.chaquo.python import Python
+
+
+
+
+sys.path
+sys.path.append('/storage/emulated/0/Android/data/com.example.platform_min_41/files/input/Code')
+
+
+
+import main_platform
